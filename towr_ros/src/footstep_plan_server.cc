5ef778e0835edf818f5217ac8468692f89e9bb40
opsullivan85 <opsullivan@wpi.edu>
2025-01-27 18:00:56 -0500

    added todo list for the action execution

--- towr_ros/src/footstep_plan_server.cc
+++ towr_ros/src/footstep_plan_server.cc
@@ -27,16 +27,40 @@ public:
   {
   }
 
+  towr_ros::FootstepPlanResult execute(const towr_ros::FootstepPlanGoalConstPtr &goal)
+  {
+    // implement code from:
+    // https://github.com/opsullivan85/RBE550-Group-Project/blob/main/src/quadruped_drake/towr/trunk_mpc.cpp
+    // - replace argv arguments with goal arguments
+    // - replace std::cout with ROS_INFO, etc
+    // - remove unnecessary arguments - gait_type, optimize_gait, etc
+    // - replace formulation.terrain_ = std::make_shared<Grid>(grid_csv); with my implementation (grid_height_map.h)
+    // - ensure the correct robot model is used
+    // - figure out how get output in the correct format (towr_ros::FootstepPlanResult)
+    //   - make another function to extract the footstep planes from the SRB trajectory and terrain
+
+    return towr_ros::FootstepPlanResult();
+  }
+
   void executeCB(const towr_ros::FootstepPlanGoalConstPtr &goal)
   {
     ROS_INFO("%s: Executing footstep planner", action_name_.c_str());
 
-    // TODO: Call towr
-    // result_ = towr.doStuff();
+    try
+    {
+      result_ = execute(goal);
+    }
+    catch(const std::exception& e)
+    {
+      as_.setAborted(result_, e.what());
+      ROS_INFO("%s: Footstep planner aborted", action_name_.c_str());
+      return;
+    }
 
     // Set the action state to succeeded
     as_.setSucceeded(result_);
     ROS_INFO("%s: Footstep planner succeeded", action_name_.c_str());
+    return;
   }
 
 };
